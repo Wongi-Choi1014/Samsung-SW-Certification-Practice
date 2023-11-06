8fc76da85d19bc58da71d5dc9c812b8b18a5202a
wongi.choi <dnjsrl3690@naver.com>
2023-11-06 23:02:05 +0900

    231106 #2 1. Re Commit

---
+++ Codepass_ADV_Q57.py
@@ -0,0 +1,60 @@
+# %%
+import copy
+
+N = int(input())
+
+Arr = [[0 for _ in range(N)]for _ in range(N)]
+
+result = []
+
+def set_queen(Arr, row, col, N):
+    for i in range(N):
+        #if Arr[row][i] == 0:
+        Arr[row][i] = -1
+        #if Arr[i][col] == 0:
+        Arr[i][col] = -1
+        if row-i>=0:
+            if col - i >= 0:
+                #if Arr[row - i][col - i] == 0:
+                Arr[row - i][col - i] = -1
+            if col + i < N:
+                #if Arr[row - i][col + i] == 0:
+                Arr[row - i][col + i] = -1
+        if row + i < N:
+            if col - i >= 0:
+                #if Arr[row+i][col-i] == 0:
+                Arr[row+i][col-i] = -1
+            if col + i < N:
+                #if Arr[row+i][col+i] == 0:
+                Arr[row+i][col+i] = -1
+
+
+
+def Back_Tracking(Arr, row, col, cnt, N, result):
+    for r in range(row, N):
+        for c in range(N):
+            #if row == r and c<col:
+            #    continue
+            Arr_search = copy.deepcopy(Arr)
+
+            if Arr_search[r][c] == 0:
+                Arr_search[r][c] = 1
+                set_queen(Arr_search, r, c, N)
+                if cnt+1 == N:
+                    result.append(1)
+                    return
+            #if cnt == N:
+            #    return
+                if c == N-1 and r == N-1:
+                    return
+                #if c < N-1:
+                #    Back_Tracking(Arr_search, r, c+1, cnt+1, N, result)
+                #else:
+                #    Back_Tracking(Arr_search, r+1 , 0, cnt+1, N, result)
+                Back_Tracking(Arr_search, r+1, 0, cnt+1, N, result)
+
+Back_Tracking(Arr, 0, 0, 0, N, result)
+print(len(result))
+
+
+# %%
