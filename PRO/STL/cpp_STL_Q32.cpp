9a10c373b1ea82b9acaad6dc7a466e4fd5b6be57
wongi.choi <dnjsrl3690@naver.com>
2024-04-28 20:23:53 +0900

    240428 변경사항 #2 1. Q32 문제 최적화

--- PRO/STL/cpp_STL_Q32.cpp
+++ PRO/STL/cpp_STL_Q32.cpp
@@ -21,39 +21,62 @@ int activate(string ID)
 
 int signup(string ID)
 {
-    if(DB.count(ID)==0)
-        DB.insert({ID,0});
+    //if(DB.count(ID)==0)
+    DB.insert({ID,0});
     return DB.size();
 }
 
 int close(string ID)
 {
+    auto it = DB.find(ID);
+    if(it!=DB.end())
+    {
+        if(it->second==1)
+            current_login-=1;
+        DB.erase(ID);
+    }
+    /*
     if(DB.count(ID)==1)
     {
         if(DB[ID]==1)
             current_login-=1;
         DB.erase(ID);
     }
-
+    */
     return DB.size();
 }
 int login(string ID)
 {
+    auto it = DB.find(ID);
+    if(it!=DB.end() && it->second==0)
+    {
+        it->second=1;
+        current_login+=1;
+    }
+    /*
     if(DB.count(ID)==1 && DB[ID]==0)
     {
         DB[ID]=1;
         current_login+=1;
     }
-
+    */
     return current_login;
 }
 int logout(string ID)
 {
+    auto it = DB.find(ID);
+    if(it!=DB.end() && it->second==1)
+    {
+        it->second=0;
+        current_login-=1;
+    }
+    /*
     if(DB.count(ID)==1 && DB[ID]==1)
     {
         DB[ID]=0;
         current_login-=1;
     }
+    */
     return current_login;
 }
 
